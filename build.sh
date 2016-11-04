c291963d6cacca26e56d61ef2150b4bbc98702e3
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-11-04 08:40:47 +0530

    Initial commit
    build.sh, shell script to --/build/-- whole "d3-start-up®" in one go.
    Usage: $ sh build.sh

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+
+# =========================== I'm Prabhat® ===========================
+#               Technical Architect & Data Scientist
+#                             d3-start-up®
+#                               ~~~~~~~~
+#       Handcrafted by Prabhat Kumar, http://prabhatkumar.org/.
+#        Copyright © 2016, Prabhat Kumar, All rights reserved.
+# Copyright © 2014 - 2016, Sequømics Corporation. All rights reserved.
+# ====================================================================
