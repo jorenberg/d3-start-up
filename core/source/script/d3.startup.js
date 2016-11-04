8ac62c1f36150f2d94bc4cb3c267d1ea4b4eae30
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-11-04 09:11:40 +0530

    Initial commit
    A ready to go scaffolding to work on d3 » Build: SEED™: Umeå.

---
+++ core/source/script/d3.startup.js
@@ -0,0 +1,18 @@
+/*!
+ * d3-start-up®
+ * A ready to go scaffolding to work on d3.
+ * ____________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Sequømics Corporation [http://sequomics.com/].
+ * ____________________________________________________________________
+ * @date      : 04-Nov-2016
+ * @license   : MIT
+ * @require   : d3.js®
+ * @require   : Modernizr
+ * @build     : SEED™: Umeå
+ *              |---- A Sequømics Product.
+ * ____________________________________________________________________
+ */
