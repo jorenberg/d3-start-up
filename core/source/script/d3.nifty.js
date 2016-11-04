dcf7297eb196dc7de1efc897cbf9f21498fa8351
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-11-04 09:19:16 +0530

    Initial commit
    A Nifty convenience functions to speed up working with d3.js » Build: SEED™: Umeå.

---
+++ core/source/script/d3.nifty.js
@@ -0,0 +1,8 @@
+/*!
+ * @file — d3.nifty.js
+ * @date - 04-11-2016
+ * @version — 1.0.0
+ * @require — d3.js®
+ * @author — Prabhat Kumar <prabhat.genome@gmail.com> (http://prabhatkumar.org/)
+ * @license — MIT License
+ */
