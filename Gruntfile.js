02a9766f571300fe96b40fee2314a180c4011428
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-11-04 08:50:19 +0530

    Initial commit
    --/The Heart of Build System/-- of "d3-start-up®".

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,1 +1,28 @@
 #!/usr/bin/env node
+
+
+/*!
+ * d3-start-up®
+ * A ready to go scaffolding to work on d3.
+ * ____________________________________________________________________
+ * Grunt, http://gruntjs.com/.
+ * The JavaScript Task Runner.
+ * ____________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Sequømics Corporation [http://sequomics.com/].
+ * ____________________________________________________________________
+ * @date      : 04-Nov-2016
+ * @license   : MIT
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : grunt-cli
+ * @build     : SEED™: Umeå
+ *              |---- A Sequømics Product.
+ * ____________________________________________________________________
+ *
+ * --/The Heart of Build System/-- of "d3-start-up®".
+ * ____________________________________________________________________
+ */
